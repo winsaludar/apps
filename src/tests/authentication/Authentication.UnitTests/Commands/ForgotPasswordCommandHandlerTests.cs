5e034c783e9a32808cc75c36e3c17deddef90fa2
Erwin Saludar <erwinsaludar@gmail.com>
2025-01-07 23:06:15 +0800

    add forgot and reset password unit tests

---
+++ src/tests/authentication/Authentication.UnitTests/Commands/ForgotPasswordCommandHandlerTests.cs
@@ -0,0 +1,72 @@
+﻿namespace Authentication.UnitTests.Commands;
+
+public class ForgotPasswordCommandHandlerTests
+{
+    private readonly Mock<IUserRepository> _userRepository = new();
+    private readonly Mock<ITokenRepository> _tokenRepository = new();
+    private readonly Mock<IEmailService> _emailService = new();
+    private readonly ForgotPasswordCommandHandler _handler;
+
+    public ForgotPasswordCommandHandlerTests()
+    {
+        _handler = new(_userRepository.Object, _tokenRepository.Object, _emailService.Object);
+    }
+
+    [Fact]
+    public async Task Handle_EmailDoesNotExist_ThrowsBadRequestException()
+    {
+        // Arrange
+        ForgotPasswordCommand command = new("email@example.com");
+        _userRepository.Setup(x => x.GetByEmailAsync(It.IsAny<string>()))
+            .ReturnsAsync((User)null!);
+
+        // Act & Assert
+        await Assert.ThrowsAsync<BadRequestException>(() => _handler.Handle(command, CancellationToken.None));
+    }
+
+    [Fact]
+    public async Task Handle_EmailNotVerified_ThrowsBadRequestException()
+    {
+        // Arrange
+        ForgotPasswordCommand command = new("email@example.com");
+        User user = User.Create("username", command.Email, Guid.NewGuid(), false);
+        _userRepository.Setup(x => x.GetByEmailAsync(It.IsAny<string>()))
+            .ReturnsAsync(user);
+
+        // Act & Assert
+        await Assert.ThrowsAsync<BadRequestException>(() => _handler.Handle(command, CancellationToken.None));
+    }
+
+    [Fact]
+    public async Task Handle_GeneratedTokenIsEmpty_ThrowsTokenException()
+    {
+        // Arrange
+        ForgotPasswordCommand command = new("email@example.com");
+        User user = User.Create("username", command.Email, Guid.NewGuid(), true);
+        _userRepository.Setup(x => x.GetByEmailAsync(It.IsAny<string>()))
+            .ReturnsAsync(user);
+        _tokenRepository.Setup(x => x.GeneratePasswordResetTokenAsync(It.IsAny<User>()))
+            .ReturnsAsync((string)null!);
+
+        // Act & Assert
+        await Assert.ThrowsAsync<TokenException>(() => _handler.Handle(command, CancellationToken.None));
+    }
+
+    [Fact]
+    public async Task Handle_ForgotPasswordSuccessful_SendEmail()
+    {
+        // Arrange
+        ForgotPasswordCommand command = new("email@example.com");
+        User user = User.Create("username", command.Email, Guid.NewGuid(), true);
+        _userRepository.Setup(x => x.GetByEmailAsync(It.IsAny<string>()))
+            .ReturnsAsync(user);
+        _tokenRepository.Setup(x => x.GeneratePasswordResetTokenAsync(It.IsAny<User>()))
+            .ReturnsAsync("token");
+
+        // Act
+        var result = await _handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        _emailService.Verify(x => x.SendForgotPasswordUrl(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()), Times.Once());
+    }
+}
---
+++ src/tests/authentication/Authentication.UnitTests/Validators/ForgotPasswordCommandValidatorTests.cs
@@ -0,0 +1,34 @@
+﻿namespace Authentication.UnitTests.Validators;
+
+public class ForgotPasswordCommandValidatorTests
+{
+    private readonly ForgotPasswordCommandValidator _validator = new();
+
+    [Fact]
+    public async Task Email_IsEmpty_ReturnsAnError()
+    {
+        // Arrange
+        ForgotPasswordCommand command = new("");
+
+        // Act
+        var result = await _validator.ValidateAsync(command);
+
+        // Assert
+        Assert.NotEmpty(result.Errors);
+        Assert.True(result.Errors?.Any(x => x.PropertyName == nameof(ForgotPasswordCommand.Email)));
+    }
+
+    [Fact]
+    public async Task Email_IsInvalidFormat_ReturnsAnError()
+    {
+        // Arrange
+        ForgotPasswordCommand command = new("email");
+
+        // Act
+        var result = await _validator.ValidateAsync(command);
+
+        // Assert
+        Assert.NotEmpty(result.Errors);
+        Assert.True(result.Errors?.Any(x => x.PropertyName == nameof(ForgotPasswordCommand.Email)));
+    }
+}
