2f5bec365009ae1add6d43a65ce59a39f7662ea4
Erwin Saludar <erwinsaludar@gmail.com>
2025-01-07 21:53:53 +0800

    add confirm email unit tests

---
+++ src/tests/authentication/Authentication.UnitTests/Validators/ConfirmEmailCommandValidatorTests.cs
@@ -0,0 +1,50 @@
+﻿using FluentValidation;
+
+namespace Authentication.UnitTests.Validators;
+
+public class ConfirmEmailCommandValidatorTests
+{
+    private readonly ConfirmEmailCommandValidator _validator = new();
+
+    [Fact]
+    public async Task Email_IsEmpty_ReturnsAnError()
+    {
+        // Arrange
+        ConfirmEmailCommand command = new("", "token");
+
+        // Act
+        var result = await _validator.ValidateAsync(command);
+
+        // Assert
+        Assert.NotEmpty(result.Errors);
+        Assert.True(result.Errors?.Any(x => x.PropertyName == nameof(ConfirmEmailCommand.Email)));
+    }
+
+    [Fact]
+    public async Task Email_IsInvalidFormat_ReturnsAnError()
+    {
+        // Arrange
+        ConfirmEmailCommand command = new("email", "token");
+
+        // Act
+        var result = await _validator.ValidateAsync(command);
+
+        // Assert
+        Assert.NotEmpty(result.Errors);
+        Assert.True(result.Errors?.Any(x => x.PropertyName == nameof(ConfirmEmailCommand.Email)));
+    }
+
+    [Fact]
+    public async Task Token_IsEmpty_ReturnsAnError()
+    {
+        // Arrange
+        ConfirmEmailCommand command = new("email@example.com", "");
+
+        // Act
+        var result = await _validator.ValidateAsync(command);
+
+        // Assert
+        Assert.NotEmpty(result.Errors);
+        Assert.True(result.Errors?.Any(x => x.PropertyName == nameof(ConfirmEmailCommand.Token)));
+    }
+}
