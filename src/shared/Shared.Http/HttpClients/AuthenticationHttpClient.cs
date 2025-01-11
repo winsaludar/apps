15579655a906f927c771ea112d9c3f71bf486b89
Erwin Saludar <erwinsaludar@gmail.com>
2025-01-11 20:51:00 +0800

    refactor authentication http client

--- src/shared/Shared.Http/HttpClients/AuthenticationHttpClient.cs
+++ src/shared/Shared.Http/HttpClients/AuthenticationHttpClient.cs
@@ -1,175 +1,74 @@
 ﻿namespace Shared.Http.HttpClients;
 
-public class AuthenticationHttpClient(HttpClient httpClient, AuthenticationApiSettings authApiSettings) : IAuthenticationHttpClient
+public class AuthenticationHttpClient(HttpClient httpClient, AuthenticationApiSettings authApiSettings) : BaseHttpClient(httpClient), IAuthenticationHttpClient
 {
-    private const string FRIENDLY_ERROR = "Something went wrong, please try again later.";
-    private readonly JsonSerializerOptions _jsonSerializerOptions = new() { PropertyNameCaseInsensitive = true };
-
     public async Task<ClientResponse> LoginAsync(string email, string password)
     {
-        try
+        string url = $"{authApiSettings.BaseUrl}{authApiSettings.LoginRoute}";
+        var payload = new { email, password };
+
+        return await PostAsJsonAsync<LoginResponse>(url, payload, (response) =>
         {
-            string url = $"{authApiSettings.BaseUrl}{authApiSettings.LoginRoute}";
-            var payload = new { email, password };
-
-            HttpResponseMessage response = await httpClient.PostAsJsonAsync(url, payload);
-            string responseContent = await response.Content.ReadAsStringAsync();
-            BaseResponse? result = response.IsSuccessStatusCode
-                ? JsonSerializer.Deserialize<LoginResponse?>(responseContent, _jsonSerializerOptions)
-                : JsonSerializer.Deserialize<ErrorResponse?>(responseContent, _jsonSerializerOptions);
-
-            return result switch
+            if (response is ErrorResponse errorResponse)
             {
-                ErrorResponse errorResult when !response.IsSuccessStatusCode => new ClientResponse<TokenDto>
-                {
-                    IsSuccessful = false,
-                    Errors = errorResult.Details
-                },
-                LoginResponse successResult => new ClientResponse<TokenDto>
-                {
-                    IsSuccessful = true,
-                    Data = successResult.Token
-                },
-                _ => new ClientResponse<TokenDto>
-                {
-                    IsSuccessful = false,
-                    Errors = [FRIENDLY_ERROR]
-                },
-            };
-        }
-        catch (Exception)
-        {
-            return new ClientResponse { IsSuccessful = false, Errors = [FRIENDLY_ERROR] };
-        }
+                return new ClientResponse { IsSuccessful = false, Errors = errorResponse.Details };
+            }
+            else
+            {
+                TokenDto? token = (response as LoginResponse)?.Token;
+                return new ClientResponse<TokenDto> { IsSuccessful = true, Data = token };
+            }
+        });
     }
 
     public async Task<ClientResponse> RegisterAsync(string username, string email, string password, string retypePassword)
     {
-        try
+        string url = $"{authApiSettings.BaseUrl}{authApiSettings.RegisterRoute}";
+        var payload = new { username, email, password, retypePassword };
+
+        return await PostAsJsonAsync<RegisterResponse>(url, payload, (response) => 
         {
-            string url = $"{authApiSettings.BaseUrl}{authApiSettings.RegisterRoute}";
-            var payload = new { username, email, password, retypePassword };
-
-            HttpResponseMessage response = await httpClient.PostAsJsonAsync(url, payload);
-            string responseContent = await response.Content.ReadAsStringAsync();
-            BaseResponse? result = response.IsSuccessStatusCode
-                ? JsonSerializer.Deserialize<RegisterResponse?>(responseContent, _jsonSerializerOptions)
-                : JsonSerializer.Deserialize<ErrorResponse?>(responseContent, _jsonSerializerOptions);
-
-            return result switch
+            if (response is ErrorResponse errorResponse)
             {
-                ErrorResponse errorResult when !response.IsSuccessStatusCode => new ClientResponse<string>
-                {
-                    IsSuccessful = false,
-                    Errors = errorResult.Details
-                },
-                RegisterResponse successResult => new ClientResponse<string>
-                {
-                    IsSuccessful = true,
-                    Data = successResult.Id.ToString()
-                },
-                _ => new ClientResponse<string>
-                {
-                    IsSuccessful = false,
-                    Errors = [FRIENDLY_ERROR]
-                },
-            };
-        }
-        catch (Exception)
-        {
-            return new ClientResponse { IsSuccessful = false, Errors = [FRIENDLY_ERROR] };
-        }
+                return new ClientResponse { IsSuccessful = false, Errors = errorResponse.Details };
+            }
+            else
+            {
+                string? userId = (response as RegisterResponse)?.Id.ToString();
+                return new ClientResponse<string> { IsSuccessful = true, Data = userId };
+            }
+        });
     }
 
     public async Task<ClientResponse> ConfirmEmailAsync(string email, string token)
     {
-        try
-        {
-            string url = $"{authApiSettings.BaseUrl}{authApiSettings.ConfirmEmailRoute}";
-            var payload = new { email, token };
+        string url = $"{authApiSettings.BaseUrl}{authApiSettings.ConfirmEmailRoute}";
+        var payload = new { email, token };
 
-            HttpResponseMessage response = await httpClient.PostAsJsonAsync(url, payload);
-            string responseContent = await response.Content.ReadAsStringAsync();
-            BaseResponse? result = response.IsSuccessStatusCode
-                ? JsonSerializer.Deserialize<SuccessResponse?>(responseContent, _jsonSerializerOptions)
-                : JsonSerializer.Deserialize<ErrorResponse?>(responseContent, _jsonSerializerOptions);
-
-            return (result is ErrorResponse errorResult)
-                ? new() { IsSuccessful = false, Errors = errorResult.Details }
-                : new() { IsSuccessful = true };
-        }
-        catch (Exception)
-        {
-            return new ClientResponse { IsSuccessful = false, Errors = [FRIENDLY_ERROR] };
-        }
+        return await PostAsJsonAsync(url, payload);
     }
 
     public async Task<ClientResponse> ResendEmailConfirmationAsync(string email)
     {
-        try
-        {
-            string url = $"{authApiSettings.BaseUrl}{authApiSettings.ResendEmailConfirmationRoute}";
-            var payload = new { email };
+        string url = $"{authApiSettings.BaseUrl}{authApiSettings.ResendEmailConfirmationRoute}";
+        var payload = new { email };
 
-            HttpResponseMessage response = await httpClient.PostAsJsonAsync(url, payload);
-            string responseContent = await response.Content.ReadAsStringAsync();
-            BaseResponse? result = response.IsSuccessStatusCode
-                ? JsonSerializer.Deserialize<SuccessResponse?>(responseContent, _jsonSerializerOptions)
-                : JsonSerializer.Deserialize<ErrorResponse?>(responseContent, _jsonSerializerOptions);
-
-            return (result is ErrorResponse errorResult)
-                ? new() { IsSuccessful = false, Errors = errorResult.Details }
-                : new() { IsSuccessful = true };
-        }
-        catch (Exception)
-        {
-            return new ClientResponse { IsSuccessful = false, Errors = [FRIENDLY_ERROR] };
-        }
+        return await PostAsJsonAsync(url, payload);
     }
 
     public async Task<ClientResponse> ForgotPasswordAsync(string email)
     {
-        try
-        {
-            string url = $"{authApiSettings.BaseUrl}{authApiSettings.ForgotPasswordRoute}";
-            var payload = new { email };
+        string url = $"{authApiSettings.BaseUrl}{authApiSettings.ForgotPasswordRoute}";
+        var payload = new { email };
 
-            HttpResponseMessage response = await httpClient.PostAsJsonAsync(url, payload);
-            string responseContent = await response.Content.ReadAsStringAsync();
-            BaseResponse? result = response.IsSuccessStatusCode
-                ? JsonSerializer.Deserialize<SuccessResponse?>(responseContent, _jsonSerializerOptions)
-                : JsonSerializer.Deserialize<ErrorResponse?>(responseContent, _jsonSerializerOptions);
-
-            return (result is ErrorResponse errorResult)
-                ? new() { IsSuccessful = false, Errors = errorResult.Details }
-                : new() { IsSuccessful = true };
-        }
-        catch (Exception)
-        {
-            return new ClientResponse { IsSuccessful = false, Errors = [FRIENDLY_ERROR] };
-        }
+        return await PostAsJsonAsync(url, payload);
     }
 
     public async Task<ClientResponse> ResetPasswordAsync(string email, string password, string retypePassword, string token)
     {
-        try
-        {
-            string url = $"{authApiSettings.BaseUrl}{authApiSettings.ResetPasswordRoute}";
-            var payload = new { email, password, retypePassword, token };
+        string url = $"{authApiSettings.BaseUrl}{authApiSettings.ResetPasswordRoute}";
+        var payload = new { email, password, retypePassword, token };
 
-            HttpResponseMessage response = await httpClient.PostAsJsonAsync(url, payload);
-            string responseContent = await response.Content.ReadAsStringAsync();
-            BaseResponse? result = response.IsSuccessStatusCode
-                ? JsonSerializer.Deserialize<SuccessResponse?>(responseContent, _jsonSerializerOptions)
-                : JsonSerializer.Deserialize<ErrorResponse?>(responseContent, _jsonSerializerOptions);
-
-            return (result is ErrorResponse errorResult)
-                ? new() { IsSuccessful = false, Errors = errorResult.Details }
-                : new() { IsSuccessful = true };
-        }
-        catch (Exception)
-        {
-            return new ClientResponse { IsSuccessful = false, Errors = [FRIENDLY_ERROR] };
-        }
+        return await PostAsJsonAsync(url, payload);
     }
 }
---
+++ src/shared/Shared.Http/HttpClients/BaseHttpClient.cs
@@ -0,0 +1,48 @@
+﻿namespace Shared.Http.HttpClients;
+
+public abstract class BaseHttpClient(HttpClient httpClient)
+{
+    private readonly JsonSerializerOptions _jsonSerializerOptions = new() { PropertyNameCaseInsensitive = true };
+
+    protected async Task<ClientResponse> PostAsJsonAsync(string url, object payload)
+    {
+        return await PostAsJsonAsync<SuccessResponse, ErrorResponse>(url, payload, (response) =>
+        {
+            return (response is ErrorResponse errorResponse)
+                ? new() { IsSuccessful = false, Errors = errorResponse.Details }
+                : new() { IsSuccessful = true };
+        });
+    }
+
+    protected async Task<ClientResponse> PostAsJsonAsync<T>(string url, object payload, Func<BaseResponse, ClientResponse> processResponse)
+    {
+        return await PostAsJsonAsync<T, ErrorResponse>(url, payload, processResponse);
+    }
+
+    protected async Task<ClientResponse> PostAsJsonAsync<T, U>(string url, object payload, Func<BaseResponse, ClientResponse> processResponse)
+    {
+        try
+        {
+            HttpResponseMessage response = await httpClient.PostAsJsonAsync(url, payload);
+            string responseContent = await response.Content.ReadAsStringAsync();
+
+            dynamic? result = response.IsSuccessStatusCode
+                ? JsonSerializer.Deserialize<T>(responseContent, _jsonSerializerOptions)
+                : JsonSerializer.Deserialize<U>(responseContent, _jsonSerializerOptions);
+
+            return processResponse(result);
+        }
+        catch (TaskCanceledException ex)
+        {
+            // Handle request cancellation or timeouts
+            Console.Error.WriteLine($"Request timeout: {ex.Message}");
+            return new() { IsSuccessful = false, Errors = ["The request timed out. Please try again later."] };
+        }
+        catch (Exception ex)
+        {
+            // Handle any other exceptions
+            Console.Error.WriteLine($"Unexpected error: {ex.Message}");
+            return new() { IsSuccessful = false, Errors = ["Something went wrong, please try again later."] };
+        }
+    }
+}
