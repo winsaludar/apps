637f07cd3bcfb18cee492540f930635b9e078f23
Erwin Saludar <erwinsaludar@gmail.com>
2025-01-20 17:20:57 +0800

    add update expense unit tests

--- src/tests/budget/Budget.UnitTests/GlobalUsings.cs
+++ src/tests/budget/Budget.UnitTests/GlobalUsings.cs
@@ -2,6 +2,7 @@ global using Budget.Application.Expenses;
 global using Budget.Application.Expenses.Create;
 global using Budget.Application.Expenses.GetAll;
 global using Budget.Application.Expenses.GetById;
+global using Budget.Application.Expenses.Update;
 global using Budget.Domain.Expenses;
 global using Moq;
 global using Shared.Common.Exceptions;
